0eeabcf2ab24853d42ba759fa6cf5538ca32dadb
SFerguson90 <scott.ferguson383@gmail.com>
2021-12-06 21:04:47 -0600

    Initial commit

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pages',
+    'posts',
 ]
 
 MIDDLEWARE = [
@@ -108,13 +109,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
