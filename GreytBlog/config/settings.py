f43bfbed1b8e9a08a635d6946011c1d72c4bffdc
SFerguson90 <Scott.Ferguson383@gmail.com>
2021-12-12 00:14:32 -0600

    small css file. modded html file

--- GreytBlog/config/settings.py
+++ GreytBlog/config/settings.py
@@ -106,13 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
@@ -120,3 +116,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
